9fc8df65526cf0b3e409c3e2670d874258dc34bc
yunju <ch00251@naver.com>
2021-05-20 20:54:29 +0900

    Step05_Login 에 로그인 기능 추가

--- Step05_Login/src/test/users/dao/UsersDao.java
+++ Step05_Login/src/test/users/dao/UsersDao.java
@@ -2,6 +2,7 @@ package test.users.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import test.users.dto.UsersDto;
 import test.util.DbcpBean;
@@ -15,6 +16,37 @@ public class UsersDao {
 		}
 		return dao;
 	}
+	//인자로 전달되는 UsersDto에 담긴 정보가 유효한 정보인지 여부를 리턴해주는 메소드
+	public boolean isValid(UsersDto dto) {
+		boolean isValid=false;
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		try {
+			conn=new DbcpBean().getConn();
+			String sql="select * from users where id=? and pwd=?";
+			pstmt=conn.prepareStatement(sql);
+			//?에 값 바인딩하기
+			pstmt.setString(1, dto.getId());
+			pstmt.setString(2, dto.getPwd());
+			rs=pstmt.executeQuery();
+			while(rs.next()) {//select된 row가 있으면
+				//아이디, 비밀번호가 일치함으로 isValid=true로 바꿔준다
+				isValid=true;
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch (Exception e) {				
+			}
+		}
+		return isValid;
+	}
+	
 	//회원 가입 정보를 DB에 저장하는 메소드
 	public boolean insert(UsersDto dto) {
 		Connection conn=null;
