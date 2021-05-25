dc06c62cd55a4ba1e63ad509730a581b6ec8e2ca
yunju <ch00251@naver.com>
2021-05-25 15:15:06 +0900

    Step05_Login에 글 삭제기능 추가

--- Step05_Login/src/test/cafe/dao/CafeDao.java
+++ Step05_Login/src/test/cafe/dao/CafeDao.java
@@ -18,6 +18,34 @@ public class CafeDao {
 		}
 		return dao;
 	}
+	//글 정보를 삭제하는 메소드
+	public boolean delete(int num) {
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		int flag=0;
+		try {
+			conn=new DbcpBean().getConn();
+			String sql="delete from board_cafe where num=?";
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setInt(1, num);
+			flag=pstmt.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception e) {
+				
+			}
+		}
+		if(flag>0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
 	//글 정보를 수정하는 메소드
 	public boolean update(CafeDto dto) {
 		Connection conn=null;
