42003904f1cf04547a7162018aa388fc25b453d4
yunju <ch00251@naver.com>
2021-06-07 21:06:17 +0900

    Spring02프로젝트에 오늘의 인물보기 추가

---
+++ Spring02_Hello/src/main/java/com/yunju/spring02/PersonController.java
@@ -0,0 +1,15 @@
+package com.yunju.spring02;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PersonController {
+	@RequestMapping("/person.do")
+	public String person(HttpServletRequest request) {
+		request.setAttribute("personToday", "±è±¸¶ó");
+		return "person";
+	}
+}
