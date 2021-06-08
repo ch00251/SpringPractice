a80d70be865547692f043962ab6f84d8cd120b26
yunju <ch00251@naver.com>
2021-06-08 18:50:52 +0900

    Spring03에 조건부 리다일렉트 예제 추가

--- Spring03_Basic/src/main/java/com/yunju/spring03/HomeController.java
+++ Spring03_Basic/src/main/java/com/yunju/spring03/HomeController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 /*
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Controller;
  *  - component scan을 통해서 spring bean container에서 관리가 되는 bean이 되어야 동작을 한다
  */
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController{
 	// /home.do 요청이 왔을 때 요청을 처리하게 하는 @RequestMapping 어노테이션
@@ -35,5 +37,17 @@ public class HomeController{
 		 *  응답된다.
 		 */
 		return "home";
+	}
+	
+	@RequestMapping("/play")
+	public ModelAndView play(HttpSession session,
+				ModelAndView mView) {
+		//세션에 로그인 정보가 있는지 확인한다.
+		String id=(String)session.getAttribute("id");
+		if(id==null) {
+			//로그인되지 않은 상태이면 로그인 폼으로 리다일렉트
+			mView.setViewName("redirect:/users/loginform.do");
+		}
+		return mView;
 	}
 }
