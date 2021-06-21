0c61c76e43ea2f40a0541246b12e684002555581
yunju <ch00251@naver.com>
2021-06-22 00:10:41 +0900

    Spring05의 member테이블 회원목록보기 요청처리 구현(/member/list.do)

---
+++ Spring05_Final/src/main/java/com/yunju/spring05/member/dao/MemberDaoImpl.java
@@ -0,0 +1,23 @@
+package com.yunju.spring05.member.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.yunju.spring05.member.dto.MemberDto;
+
+@Repository
+public class MemberDaoImpl implements MemberDao{
+
+	//핵심 의존 객체를 spring으로 부터 주입받기(Dependency Injection)
+	@Autowired
+	private SqlSession session;
+	
+	@Override
+	public List<MemberDto> getList(){
+		List<MemberDto> list=session.selectList("member.getList");
+		return list;
+	}
+}
