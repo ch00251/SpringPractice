34114b64519cb32a139da9c1d99a1b46029c3406
yunju <ch00251@naver.com>
2021-06-29 14:49:57 +0900

    [Spring05] /users/signup.do 요청 처리

--- Spring05_Final/src/main/java/com/yunju/spring05/users/dao/UsersDao.java
+++ Spring05_Final/src/main/java/com/yunju/spring05/users/dao/UsersDao.java
@@ -1,5 +1,8 @@
 package com.yunju.spring05.users.dao;
 
+import com.yunju.spring05.users.dto.UsersDto;
+
 public interface UsersDao {
 	public boolean isExist(String inputId);
+	public void insert(UsersDto dto);
 }
--- Spring05_Final/src/main/java/com/yunju/spring05/users/service/UsersService.java
+++ Spring05_Final/src/main/java/com/yunju/spring05/users/service/UsersService.java
@@ -2,6 +2,9 @@ package com.yunju.spring05.users.service;
 
 import java.util.Map;
 
+import com.yunju.spring05.users.dto.UsersDto;
+
 public interface UsersService {
 	public Map<String, Object> isExistId(String inputId);
+	public void addUser(UsersDto dto);
 }
