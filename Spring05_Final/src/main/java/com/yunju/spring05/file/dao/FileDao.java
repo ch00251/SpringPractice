18e701c5dea10e1b4449e1d28e0064f74687bcec
yunju <ch00251@naver.com>
2021-07-05 17:52:20 +0900

    [Spring05]/file/list.do 파일목록보기 요청처리 구현

--- Spring05_Final/src/main/java/com/yunju/spring05/file/dao/FileDao.java
+++ Spring05_Final/src/main/java/com/yunju/spring05/file/dao/FileDao.java
@@ -1,5 +1,11 @@
 package com.yunju.spring05.file.dao;
 
+import java.util.List;
+
+import com.yunju.spring05.file.dto.FileDto;
+
 public interface FileDao {
+	public int getCount();
+	public List<FileDto> getList(FileDto dto);
 
 }
--- Spring05_Final/src/main/java/com/yunju/spring05/file/dao/FileDaoImpl.java
+++ Spring05_Final/src/main/java/com/yunju/spring05/file/dao/FileDaoImpl.java
@@ -1,12 +1,28 @@
 package com.yunju.spring05.file.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.yunju.spring05.file.dto.FileDto;
 
 @Repository
 public class FileDaoImpl implements FileDao{
 	@Autowired
 	private SqlSession session;
 	
+	@Override
+	public int getCount() {
+		//업로드된 파일의 전체 갯수를 select해서
+		int count=session.selectOne("file.getCount");
+		return count;//리턴한다
+	}
+	
+	@Override
+	public List<FileDto> getList(FileDto dto){
+		List<FileDto> list=session.selectList("file.getList", dto);
+		return list;
+	}
 }
